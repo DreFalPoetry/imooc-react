1b04611f64332eb19dd7731e55da1df1857e6a8f
Racky <racky.liu@moca-tech.net>
2019-04-10 11:36:58 +0800

    state和props包括css组件的按需加载

--- src/App.js
+++ src/App.js
@@ -1,29 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {createStore} from 'redux';
-import './es6'
+import {Button,List} from 'antd-mobile'
+// import 'antd-mobile/dist/antd-mobile.css'
 
-class App extends Component {
-  render() {
+class App extends Component{
+  render(){
+    const boss = '李云龙'
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h2>独立团，团长是{boss}</h2>
+        <Yiying boss='张大苗'></Yiying>
+        <Qibinglian boss='孙德胜'></Qibinglian>
       </div>
-    );
+    ) 
+  }
+}
+
+function Qibinglian(props){
+  return <h2>骑兵连连长，{props.boss},冲压</h2>
+}
+
+class Yiying extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      solders:['虎子','柱子','王根沈']
+    }
+    // this.addSolders = this.addSolders.bind(this)
+  }
+
+  componentWillMount(){
+    console.log('将要加载')
+  }
+
+  componentDidMount(){
+    console.log('加载完毕')
+  }
+
+  
+
+  addSolders = () => {
+    console.log('add solder');
+    this.setState({
+      solders:[...this.state.solders,'新兵'+Math.random()]
+    })
+  }
+
+  render(){
+    console.log('组件正在加载')
+    return (
+      <div>
+        <h2>一营营长，{this.props.boss}</h2>
+        <Button type='primary' onClick={this.addSolders}>新兵入伍</Button>
+        <List 
+          renderHeader={()=>'士兵列表'}
+        >
+           {
+             this.state.solders.map((v,ind)=>{
+              return <List.Item key={ind}>{v}</List.Item>
+            })
+          }
+        </List>         
+      </div>
+    ) 
   }
 }
 
