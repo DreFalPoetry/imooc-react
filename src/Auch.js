93b89cad3125e84b2e0321fc9296eef0c9b3cc0f
RanranLiu111 <864749024@qq.com>
2019-04-11 15:37:43 +0800

    添加接口，设置前后端联调

--- src/Auch.js
+++ src/Auch.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import {connect} from 'react-redux';
-import { login} from './Auth.redux';
+import { login,getUserData} from './Auth.redux';
 import {Redirect } from 'react-router-dom'
 
+
 //
-@connect(state=>state.auth,{login})
+@connect(state=>state.auth,{login,getUserData})
 class Auth extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      data:{}
+    }
+  }
+  componentDidMount(){
+    this.props.getUserData()
+  }
   render(){
     return (
       <div>
+        <h2>我的名字是{this.props.user},年龄：{this.props.age}</h2>
         {this.props.isAuth ? <Redirect to='/dashboard'></Redirect> : null}
       <h2>你没有权限，需要登陆才能看到</h2>
       <button onClick={this.props.login}> 登陆</button>
