f5c30b52853478e06c89f85321bb7a5593dff2fe
RanranLiu111 <864749024@qq.com>
2019-04-23 14:03:47 +0800

    添加登陆注册后端

--- server/model.js
+++ server/model.js
@@ -1,4 +1,33 @@
 const mongoose = require('mongoose');
 //链接mongo 并且使用imooc这个集合
-const DB_URL = 'mongodb://127.0.0.1:27017/imooc'
+const DB_URL = 'mongodb://127.0.0.1:27017/imooc-data'
 mongoose.connect(DB_URL)
+
+const models = {
+  user:{
+    'user':{type:String,require:true},
+    'pwd':{type:String,require:true},
+    'type':{type:String,require:true},
+    //头像
+    'avatar':{type:String},
+    //个人简介或者职位简介
+    'desc':{type:String},
+    //职位名
+    'title':{type:String},
+    //如果是boss
+    'company':{type:String},
+    'money':{type:String}
+  },
+  chat:{
+  }
+}
+
+for(let m in models){
+  mongoose.model(m,new mongoose.Schema(models[m]))
+}
+
+module.exports = {
+  getModel:function(name){
+    return mongoose.model(name)
+  }
+}
