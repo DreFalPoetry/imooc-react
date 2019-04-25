4e429c757943eb618880a2d091588f3ae0301a9b
RanranLiu111 <864749024@qq.com>
2019-04-25 10:43:12 +0800

    设置链接socket.io进行实时通信

--- src/component/chat/index.js
+++ src/component/chat/index.js
@@ -1,16 +1,54 @@
 import React from 'react';
+import { List ,InputItem } from 'antd-mobile'
 import io from 'socket.io-client'
-
+const socket =  io('ws://localhost:9093')
 
 class Chat extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      text:'',
+      msg:[]
+    }
+  }
   componentDidMount(){
-    const socket =  io('ws://localhost:9093')
+    socket.on('recvmsg',(data)=>{
+      console.log(data)
+      this.setState({
+        msg:[...this.state.msg,data.text]
+      })
+    })
+  }
+
+  handleSubmit(){
+    socket.emit('sendmsg',{text:this.state.text})
+    this.setState({
+      text:''
+    })
   }
 
   render(){
-    console.log(this.props)
     return (
-      <h2>chat with user: {this.props.match.params.user}</h2>
+      <div>
+        {this.state.msg.map((v,index)=>{
+          return <p key={index}>{v}</p>
+        })}
+         <div className='stick-footer'>
+            <List>
+              <InputItem
+                placeholder='请输入'
+                value={this.state.text}
+                onChange={v=>{
+                  this.setState({
+                    text:v
+                  })
+                }}
+                extra={<span onClick={()=>this.handleSubmit()}>发送</span>}
+              />            
+            </List>
+          </div>
+      </div>
+      // <h2>chat with user: {this.props.match.params.user}</h2>
     )
   }
 }
